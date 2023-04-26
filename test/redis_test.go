6bb774939389370812a364a66957036eab7a56f8
zxc <15660589213@163.com>
2023-04-26 11:17:46 +0800

    redis_demo

---
+++ test/redis_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"testing"
+)
+
+var ctx = context.Background()
+
+func ExampleClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+}
+
+func TestRedis(t *testing.T) {
+	ExampleClient()
+}
